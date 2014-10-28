3e809dc28dfd8dd5a58c803bddd0ebf6e04cf660
Tobias <tobiasraabe@uni-bonn.de>
2014-10-28 14:31:03 +0100

    Verbesserung von Blatt 1, Aufgabe 3
    -unnötige Zeilen entfernt
    -Schleife verkürzt

--- Blatt1/aufgabe3.c
+++ Blatt1/aufgabe3.c
@@ -4,16 +4,17 @@
 
 int main()
 {
-	int input,i,var;
+	int input,i,var=0;
 	label1: printf("Welche Zahl soll überprüft werden, ob es sich bei ihr um eine Primzahl handelt?\n->");
 	scanf("%d", &input);
 	if(input<=0){printf("%d gehört nicht zu den natürlichen Zahlen.\n", input); goto label1;}
-	for (i=2; i<input; i++)
+	if(input==2){goto label2;}
+	if(input==3){goto label2;}
+	for (i=2; i*i<input; i++)
 	{
 		if(input%i==0){var=1; break;}
-		else{var=0;}
 	}
-	if(var==0){printf("%d ist eine Primzahl\n", input);}
+	label2: if(var==0){printf("%d ist eine Primzahl\n", input);}
 	if(var==1){printf("%d ist keine Primzahl\n", input);}
 	return 0;
 }
