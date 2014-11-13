ae5adfc8406658b1579305fabdbbad2362dcd9ae
TorbenPl <TorbenPl@users.noreply.github.com>
2014-11-13 18:59:16 +0100

    Aufgabe3.c

--- Blatt4/Aufgabe 3/aufgabe3.c
+++ Blatt4/Aufgabe 3/aufgabe3.c
@@ -6,25 +6,13 @@ int i,j, k,l; //Dimensionen der Matrizen
 int multi[i][l]=0;
 if (j!=k) printf("Die Dimensionen der Matrizen eignen sich nicht für die Multiplikation\n");
 else {
-for (int a=1; a<=j; a++){//Zeile wählen
-for (int b=1; b<=l; b++){//Spalte wähken
-for (int c=1; c<=j; c++){//Die Summe von Zeile und Spalte in die Ergebnismatrix schreiben.
-multi[a][b]=multi[a][b]+mata[a][c]*matb[c][b];
-
-}
-}
-
-}
-//for (int a=0; a<i; a++){//Zeile wählen
-//	for (int b=0; b<l; b++){//Spalte wählen
-//		for (int c=0; c<j; c++){//Die Summe von Zeile und Spalte in die Ergebnismatrix schreiben.
-//			multi[a][b]=multi[a][b]+mata[a][c]*matb[c][b];
-//		}
-//	}
-//}
-
-
-}; //Semikolon nicht nötig, da Statement schon in {} gefasst
-
-	return 0;
+  for (int a=0; a<i; a++){//Zeile wählen
+    for (int b=0; b<l; b++){//Spalte wählen
+      for (int c=0; c<j; c++){//Die Summe von Zeile und Spalte in die Ergebnismatrix schreiben.
+	multi[a][b]=multi[a][b]+mata[a][c]*matb[c][b];
+	}
+      }
+    }
+  }
+return 0;
 }
