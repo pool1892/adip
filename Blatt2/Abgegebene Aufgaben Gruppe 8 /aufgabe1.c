8c61d4ff23626da9176916ef99eaa4fe0aa76bd1
Tobias <tobiasraabe@uni-bonn.de>
2014-11-18 16:45:25 +0100

    ACHTUNG!! RÜCKGÄNGIG MACHEN, WENN FALSCH
    keine Ahnung was genau passiert ist aber das sollte ich plötzlich
    committen

--- Blatt2/Abgegebene Aufgaben Gruppe 8 /aufgabe1.c
+++
@@ -1,1 +0,0 @@
-#include <stdio.h>int main(){    char x1,x2,result;    // Beispiel 1:    x1 =  35;    x2 =  85;    result =  x1 + x2;    printf("Beispiel 1: %hi + %hi = %hi\n",x1 ,x2, result);    // Beispiel 2:    x1 =  85;    x2 =  85;    result = x1 + x2;    printf("Beispiel 2: %hi + %hi = %hi\n",x1 ,x2, result);return 0; }
