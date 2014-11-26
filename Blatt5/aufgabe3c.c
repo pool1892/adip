e9dedb34baf135a5106aab0e6dcca90898d743a8
pool1892 <schwerdtfeger@uni-bonn.de>
2014-11-26 20:13:04 +0100

    Jetzt auch Aufgabe 3c, allerdings...
    …mit Vektoreingabe im Quelltext und fixer Dimension. Dafür sind die
    Vektoren structs. Ich habe es nicht geschafft, die structs dynamisch zu
    initiieren. Aber ist, hoffentlich, auch nicht gefragt.
    Falls man dynamisch alloziieren soll, wären arrays sicher leichter zu
    handhaben.
    Immerhin sind meine Funktionen alle für beliebige Dimensionen
    definiert. Falls jemand es schafft, da was dynamisches zu bauen: ich
    wäre sehr interessiert.

--- Blatt5/aufgabe3c.c
+++ Blatt5/aufgabe3c.c
@@ -10,32 +10,62 @@ Komplex conjunct(Komplex);
 
 
 struct Vektorraum {
-	Komplex dim[3];
+	Komplex dim[3]; //////////Hier muss die Dimension natürlich angepasst werden//////////////
 };
 typedef struct Vektorraum Vektor;
-Vektor vadd(Vektor, Vektor);
-Vektor skalar(Vektor, Komplex);
-Komplex vmult (Vektor, Vektor);
+Vektor vadd(Vektor, Vektor, int);
+Vektor skalar(Vektor, Komplex, int);
+Komplex vmult (Vektor, Vektor, int);
 
 
 
 
 int main(){
+
+
+
+
+//////////////////Vektorzeugs///////////////////////////
 //Sei n die Dimension des komplexen Vektorraums
+int n=3;
+Komplex scaling={2,0}; /////////////Skalar///////////////////
+Vektor test1, test2, test3, test4;
+Komplex prod;
 
-Vektor test1;
+////////////////////////////Vektoren festlegen für 3 Dimensionen/////////
 test1.dim[0].real=1;
-test1.dim[0].imag=1;
+test1.dim[0].imag=2;
 test1.dim[1].real=1;
-test1.dim[1].imag=1;
-test1.dim[2].real=1;
-test1.dim[2].imag=1;
+test1.dim[1].imag=3;
+test1.dim[2].real=2;
+test1.dim[2].imag=4;
 
+test2.dim[0].real=2;
+test2.dim[0].imag=2;
+test2.dim[1].real=2;
+test2.dim[1].imag=2;
+test2.dim[2].real=2;
+test2.dim[2].imag=2;
 
+test3=vadd(test1,test2, n);
+test4=skalar(test1, scaling, n);
+prod=vmult(test1, test2, n);
 
-
+printf("Vektoraddition\n");
+for(int count =0; count<n; count++){
+	printf("Dimension %d: (%lf + %lf i)\n", count+1, test3.dim[count].real, test3.dim[count].imag);
 }
 
+printf("Skalarmultiplikation\n");
+for(int count =0; count<n; count++){
+	printf("Dimension %d: (%lf + %lf i)\n", count+1, test4.dim[count].real, test4.dim[count].imag);
+}
+printf("Vektormultiplikation\n");
+printf("Das Ergebnis ist ( %lf + %lf i)\n", prod.real, prod.imag );
+}
+
+
+///////////////////////Komplexzahlfunktionen//////////////////////////////////////////
 Komplex add(Komplex i, Komplex j){
 Komplex result={0,0};
 result.real=i.real+j.real;
@@ -43,13 +73,18 @@ result.imag=i.imag+j.imag;
 
 return result;
 };
+
+
+
 Komplex sub(Komplex i, Komplex j){
 Komplex result={0,0};
 result.real=i.real-j.real;
 result.imag=i.imag-j.imag;
 
 return result;
-};
+}
+
+
 Komplex mult(Komplex i, Komplex j){
 Komplex result={0,0};
 result.real=i.real*j.real-i.imag*j.imag;
@@ -74,25 +109,31 @@ return result;
 
 }
 
-
-Vektor vadd( Vektor i, Vektor j){
+/////////////////////////Vektorfunktionen//////////////////////////////
+Vektor vadd( Vektor i, Vektor j,int n){
 Vektor result;
+for (int l=0;l<n; l++){
+	result.dim[l].real=i.dim[l].real+j.dim[l].real;
+	result.dim[l].imag=i.dim[l].imag+j.dim[l].imag;
+}
+return result;
+}
+Vektor skalar(Vektor i, Komplex j, int n){
+Vektor result;
+for(int count =0; count<n; count++){
+result.dim[count]=mult(j,i.dim[count]);
 
-
+}
 
 return result;
 }
-Vektor skalar(Vektor i, Komplex j){
-Vektor result;
 
-
-
-return result;
-}
-
-Komplex vmult (Vektor i, Vektor j){
+Komplex vmult (Vektor i, Vektor j, int n){
 Komplex result;
+for(int count =0; count<n; count++){
+result=add(result, mult(i.dim[count], j.dim[count]));
 
+}
 
 return result;	
 }
